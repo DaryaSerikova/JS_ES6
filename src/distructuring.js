a6f3cc8b7fb585868494972163222410858b6087
Darya Serikova <dvserikova@gmail.com>
2022-02-11 18:08:14 +0700

    added distructuring.js

---
+++ src/distructuring.js
@@ -0,0 +1,67 @@
+let [js, php, py, rb] = ['JavaScipt', 'PHP', 'Python', 'Ruby'];
+console.log(js, php, py, rb);
+
+
+
+let yes = 'Yes';
+let no = 'No';
+
+[yes, no] = [no, yes];
+console.log(`Yes is ${yes}`);
+console.log(`No is ${no}`);
+console.log('O_o');
+
+
+
+
+
+// let scores = [3, 4, [5, 6]];
+// let[low, mid, [high, higher]] = scores;
+
+// console.log(low, mid, high, higher);
+
+// function computeScore([low, mid]) {
+//     console.log(low, mid);
+// }
+
+// computeScore([3, 4]);
+
+
+
+
+// function getScores() {
+//     return [3, 4, 5];
+// }
+
+// let scores = getScores();
+
+// let [low, mid, high] = getScores();
+// console.log(low, mid, high);
+
+
+
+
+
+// let scores = [3, 4];
+// let[low, mid, high = 5] = scores;
+
+// console.log(low, mid, high);
+
+
+
+
+// let scores = [3, 4, 5];
+// let[low, ...rest] = scores;
+
+// console.log(rest);
+
+
+
+
+// let scores = [3, 4, 5];
+// let[low, mid, high] = scores;
+
+// let[l, ,h] = scores; 
+
+// console.log(low, mid, high);
+// console.log(l, h);
