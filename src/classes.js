f71d690a8865ff904f9c33520c74cbebe49602ed
Darya Serikova <dvserikova@gmail.com>
2022-02-11 13:12:22 +0700

    add complete

--- src/classes.js
+++ src/classes.js
@@ -1,11 +1,42 @@
 class Task {
-    constructor() {
+    constructor(title = Task.getDefaultTitle()) {
+        this.title = title;
+        this._done = false;
+        // Task.count += 1;
         console.log('Создание задачи');
+    }
+
+    // get done() {
+    //     return this._done === true ? 'Задача выполнена' : 'Задача не выполнена';
+    // }
+
+    complete() {
+        this.done = true;
+        console.log(`Задача "${this.title}" выполнена`);
+    }
+
+    static getDefaultTitle() {
+        return 'Задача';
     }
 }
 
-let task = new Task();
+// Task.count = 0;
 
-console.log(typeof task);
-console.log(task instanceof Task);
+let task = new Task('Убрать комнату');
 
+console.log(task.done, task._done);
+// task.complete();
+// console.log(task.done, task._done);
+
+
+
+let task2 = new Task('Купить продукты');
+let task3 = new Task();
+
+console.log(task.title);
+console.log(task2.title);
+console.log(task3.title);
+
+// console.log(Task.count);
+
+task2.complete();
