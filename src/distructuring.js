d89681aab3551c3bdd9e192eb5080d91428e83b7
Darya Serikova <dvserikova@gmail.com>
2022-02-14 12:28:47 +0700

    added distr for object

--- src/distructuring.js
+++ src/distructuring.js
@@ -1,14 +1,55 @@
 //Деструктивное присваивание объектов
-let person = {
+
+
+
+let user = {
     firstname: 'John',
-    lastname: 'Doe'
+    lastname: 'Doe',
+    social: {
+        facebook: 'johndoe',
+        twitter: 'jdoe'
+    }
 };
 
+function getUserInfo() {
+    return {
+        firstname: 'John',
+        lastname: 'Doe',
+        social: {
+            facebook: 'johndoe',
+            twitter: 'jdoe'
+        }
+    };
+}
+
+// function post( url, {data, cache}) {
+//     console.log(data, cache);
+// }
+
+function post( url, {data: {firstname, lastname}, cache}) {
+    console.log(firstname, lastname, cache);
+}
+
+let result = post( 'api/users', {data: user, cache: false});
+
+// let person = {
+//     firstname: 'John',
+//     lastname: 'Doe'
+// };
+
+let {firstname, lastname, social: {twitter}} = getUserInfo();
+// let {firstname: first, lastname: last, social: {facebook: fb }, age = 25 } = user;
+// let {firstname: first, lastname: last, social: {facebook}, age = 25 } = user;
 // let {firstname: first, lastname: last, age = 25} = {firstname: 'John', lastname: 'Doe'};
-let {['first'+'name']: first, lastname: last, age = 25} = {firstname: 'John', lastname: 'Doe'};
+// let {['first'+'name']: first, lastname: last, age = 25} = {firstname: 'John', lastname: 'Doe'};
 // let {firstname: first, lastname: last} = person;
 
-console.log(first, last, age);
+// console.log(first, last, age);
+// console.log(first, last, facebook, age);
+// console.log(first, last, fb, age);
+console.log(firstname, lastname, twitter);
+
+
 
 // let {firstname, lastname} = person;
 // console.log(firstname, lastname);
